755e5be57f3131417bebe62afb18ecc7b72e025b
sonercirit <sonercirit@gmail.com>
2022-10-16 04:10:20 +0300

    add updating payrolls

--- src/payrolls/index.js
+++ src/payrolls/index.js
@@ -31,4 +31,26 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.patch("/:id", async (req, res) => {
+  try {
+    const { draw, subcontractor } = req.body;
+    const data = req.body;
+
+    if (draw) {
+      data.draw = { connect: { id: draw } };
+    }
+    if (subcontractor) {
+      data.subcontractor = { connect: { id: subcontractor } };
+    }
+
+    const payroll = await prisma.payroll.update({
+      where: { id: req.params.id },
+      data,
+    });
+    res.json(payroll);
+  } catch (e) {
+    handleError(e, res);
+  }
+});
+
 export default router;
